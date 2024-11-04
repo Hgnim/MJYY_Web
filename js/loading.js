b43217ffac7fe0d9ea607443dd28d1aa32db2afe
Hgnim <hgn1m@outlook.com>
2024-11-04 22:15:03 +0800

    添加功能：长时间处在加载页面时会显示“跳过资源加载”按钮，避免用户长时间等待。杂项：将服务器规则更改为文本形式

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,33 @@
 
-// LOADER
-window.onload = (event) => {
-    $("#preloader").on(500).fadeOut();
-    $(".preloader").on(600).fadeOut("slow");
-};
+var isLoadingOver = false;
+function LoadingOver() {
+    if (!isLoadingOver) {
+        isLoadingOver = true;
+        $("#preloader").on(500).fadeOut();
+        $(".preloader").on(600).fadeOut("slow");
+    }
+}
+
+//加载时间过长的话则显示跳过加载的按钮
+async function WaitTimeOut(){
+    await sleep(5000);
+    if(!isLoadingOver){
+        $("#main-ld-skipbt").fadeIn();
+    }
+}
+
+
+
+function sleep(interval) {
+    return new Promise(resolve => {
+        setTimeout(resolve, interval);
+    })
+}
+
+//window.onload = LoadingOver;
+if (document.all) {
+    window.attachEvent('onload', LoadingOver)
+}
+else {
+    window.addEventListener('load', LoadingOver, false);
+}
