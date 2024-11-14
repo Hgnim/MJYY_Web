5a43cd8c00d2b16e241949a9b65b9e027e256812
Hgnim <hgn1m@outlook.com>
2024-11-14 11:14:28 +0800

    蜘蛛网动画微调

--- js/MoreLineEffect.js
+++ js/MoreLineEffect.js
@@ -10,7 +10,7 @@
  
     function t() {
         var t = e("script"), o = t.length, i = t[o - 1];
-        return {l: o, z: n(i, "zIndex", 0), o: n(i, "opacity", .5), c: n(i, "color", "74,140,229"), n: n(i, "count", 100)}
+        return {l: o, z: n(i, "zIndex", 0), o: n(i, "opacity", .4), c: n(i, "color", "74,140,229"), n: n(i, "count", 150)}
     }
  
     function o() {
@@ -21,21 +21,21 @@
         r.clearRect(0, 0, a, c);
         var n, e, t, o, m, l;
         s.forEach(function (i, x) {
-            for (i.x += i.xa, i.y += i.ya, i.xa *= i.x > a || i.x < 0 ? -1 : 1, i.ya *= i.y > c || i.y < 0 ? -1 : 1, r.fillRect(i.x - .5, i.y - .5, 1, 1), e = x + 1; e < u.length; e++) n = u[e], null !== n.x && null !== n.y && (o = i.x - n.x, m = i.y - n.y, l = o * o + m * m, l < n.max && (n === y && l >= n.max / 2 && (i.x -= .03 * o, i.y -= .03 * m), t = (n.max - l) / n.max, r.beginPath(), r.lineWidth = t / 2, r.strokeStyle = "rgba(" + d.c + "," + (t + .2) + ")", r.moveTo(i.x, i.y), r.lineTo(n.x, n.y), r.stroke()))
+            for (i.x += i.xa, i.y += i.ya, i.xa *= i.x > a || i.x < 0 ? -1 : 1, i.ya *= i.y > c || i.y < 0 ? -1 : 1, r.fillRect(i.x - .5, i.y - .5, 1, 1), e = x + 1; e < u.length; e++) n = u[e], null !== n.x && null !== n.y && (o = i.x - n.x, m = i.y - n.y, l = o * o + m * m, l < n.max && (n === y && l >= n.max / 8 && (i.x -= .03 * o, i.y -= .03 * m), t = (n.max - l) / n.max, r.beginPath(), r.lineWidth = t / 2, r.strokeStyle = "rgba(" + d.c + "," + (t + .2) + ")", r.moveTo(i.x, i.y), r.lineTo(n.x, n.y), r.stroke()))
         }), x(i)
     }
  
     var a, c, u, m = document.createElement("canvas"), d = t(), l = "c_n" + d.l, r = m.getContext("2d"),
         x = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (n) {
             window.setTimeout(n, 1e3 / 45)
-        }, w = Math.random, y = {x: null, y: null, max: 2e4};
+        }, w = Math.random, y = {x: null, y: null, max: 75000};
     m.id = l, m.style.cssText = "position:fixed;top:0;left:0;z-index:" + d.z + ";opacity:" + d.o, e("body")[0].appendChild(m), o(), window.onresize = o, window.onmousemove = function (n) {
         n = n || window.event, y.x = n.clientX, y.y = n.clientY
     }, window.onmouseout = function () {
         y.x = null, y.y = null
     };
     for (var s = [], f = 0; d.n > f; f++) {
-        var h = w() * a, g = w() * c, v = 2 * w() - 1, p = 2 * w() - 1;
+        var h = w() * a, g = w() * c, v = 1 * w() - 1, p = 1 * w() - 1;
         s.push({x: h, y: g, xa: v, ya: p, max: 14e3})
     }
     u = s.concat([y]), setTimeout(function () {
