1aba0f5e92f54d2e9cd73168968caab3fd3b9a0f
Hgnim <hgn1m@outlook.com>
2024-12-07 22:37:26 +0800

    将转到顶部的按钮替换成了有着更多动画的MC烟花火箭

--- js/pageSmoothScroll.js
+++ js/pageSmoothScroll.js
@@ -26,22 +26,58 @@ $(function () {
             $('.sticky-navigation').removeClass('shadow-bottom').removeClass('navbar-light').addClass('navbar-dark');
         }
 
+        if(!utbFlyLock){
         // adjust scroll to top
         if (scroll >= 600) {
-            $('.scroll-top').addClass('active');
+            //$('.scroll-top').addClass('active');
+            if(utbLoc==0){
+                utb.style.animation='scroll-top_out 1s forwards';
+                utbLoc=1;
+            }
         } else {
-            $('.scroll-top').removeClass('active');
+            //$('.scroll-top').removeClass('active');
+            if(utbLoc==1){
+                utb.style.animation='scroll-top_in 1s forwards';
+                utbLoc=0;
+            }
         }
+    }
         return false;
     });
-
+    var utbFlyLock=false;
+    var utbLoc=0;
+    var utb=document.getElementById('upTopButton');
+    utb.addEventListener('mouseenter', (event) => {
+        if(!utbFlyLock && utbLoc==1){
+        utb.style.right='20px';
+        utb.style.animation='scroll-top_shak 0.5s infinite';
+    }
+    });
+    utb.addEventListener('mouseleave', (event) => {
+        if(!utbFlyLock && utbLoc==1)
+         utb.style.animation='';
+    });
     // scroll top top
     $('.scroll-top').click(function () {
+        utbFlyLock=true;
+        var utba=document.querySelector('#upTopButton');
+        function waitUtbFlyOver(){
+            utb.style.top='';
+            utb.style.bottom= '';
+            utbLoc=0;
+            utbFlyLock=false;
+            utba.removeEventListener('animationend', waitUtbFlyOver);
+        }
+        utb.style.bottom='0';
+        utb.style.top = (window.innerHeight - 20) + 'px';
+        utb.style.right= '20px';
+        utb.style.setProperty("--defTopValue", utb.style.top);
+        utba.addEventListener('animationend',waitUtbFlyOver);//监听动画执行完毕的事件
+        utb.style.animation='scroll-top_fly 1s forwards';
         $('html, body').stop().animate({
             scrollTop: 0
         }, 1000);
     });
-
     /**Theme switcher - DEMO PURPOSE ONLY */
     $('.switcher-trigger').click(function () {
         $('.switcher-wrap').toggleClass('active');
