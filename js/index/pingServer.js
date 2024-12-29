ce0a2372e1fc785704be162ff2ed9fbf14957611
Hgnim <hgn1m@outlook.com>
2024-12-29 20:51:59 +0800

    开发分支中不对服务器在线状态进行检测

--- js/index/pingServer.js
+++ js/index/pingServer.js
@@ -27,45 +27,19 @@ function pingServer_Start() {
 
         var oneShot = false;
             if (!oneShot) {
-                $.ajax({
-                    url: 'https://proxy.cors.sh/https://uapis.cn/api/mcserver?server=113.219.237.106:30303',//通过公共CORS代理，避免跨域请求错误
-                    type: "GET",
-                    headers: {
-                        'X-Requested-With': 'XMLHttpRequest',
-                        'Access-Control-Allow-Origin' : '*'
-                    },
-                    contentType: "application/json",
-                    success: function (response) {
-                        function pslLoopStop() {
-                            // 设置动画迭代次数为0，让动画在完成当前迭代后停止
-                            psl.style.animationIterationCount = 0;
-                            psl.removeEventListener('animationiteration',pslLoopStop);
-                            pingServerLoaderLock=false;
-                        }
-                        pingServerLoaderLock=true;
-                        // 监听动画迭代结束事件
-                        psl.addEventListener('animationiteration',pslLoopStop);
-
-                        if(response.code==200){
-                            psl.style.background = pss_s;
-                            pst.style.color = pss_s;
-                            pst.innerText = "Minecraft服务器在线";
-                            document.getElementById("pingServerInfo_icon").src= response.img;
-                            document.getElementById("pingServerInfo_text_player").innerText="玩家数: " + response.players;
-                            document.getElementById("pingServerInfo_text_motd1").innerText=response.motd;
-                            document.getElementById("pingServerInfo_text_motd2").innerText=response.motd2;
-                            document.getElementById("pingServerInfo").style.display="flex";
-                        }
-                        else {
+                function pslLoopStop() {
+                    // 设置动画迭代次数为0，让动画在完成当前迭代后停止
+                    psl.style.animationIterationCount = 0;
+                    psl.removeEventListener('animationiteration',pslLoopStop);
+                    pingServerLoaderLock=false;
+                }
+                pingServerLoaderLock=true;
+                // 监听动画迭代结束事件
+                psl.addEventListener('animationiteration',pslLoopStop);
+                
                             psl.style.background = pss_f;
                             pst.style.color = pss_f;
-                            if(response.code!=null){
-                            pst.innerText = "Minecraft服务器已离线";}
-                            else{
-                            pst.innerText = "MC服务器在线状态检查失败";}
-                        }
-                    },
-                });
+                            pst.innerText = "开发分支中不进行检测";
             }
         pingServerRunLock =false;
     }
