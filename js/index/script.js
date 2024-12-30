96987672278d4b0d84f41072b77c894a3e740022
Hgnim <hgn1m@outlook.com>
2024-12-30 16:23:49 +0800

    为各个页面同步了顶部导航栏；优化了顶部导航栏在触摸屏设备上的使用效果

--- js/index/script.js
+++ js/index/script.js
@@ -1,11 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-document.getElementById('navbarCollapse').addEventListener('click', function(event) {
-    var ncsb=document.getElementById('navbarCollapse_showButton');
-    if(ncsb.ariaExpanded=="true"){
-    ncsb.dispatchEvent(new MouseEvent('click', {
-        'bubbles': true,    // 是否冒泡
-        'cancelable': true  // 是否可以取消
-      }));
-    }
-});
+$(function () {
+  //替换图标
+  feather.replace();
 });
