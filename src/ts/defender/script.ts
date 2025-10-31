71cd914888666a407965db2948f5ae3cf59c52ca
Hgnim <hgn1m@outlook.com>
2025-10-31 17:49:58 +0800

    防御者小游戏完工

--- src/ts/defender/script.ts
+++ src/ts/defender/script.ts
@@ -1,16 +1,33 @@
 import {sleep} from "@/ts/global/sleep";
+import {redirectPage} from "@/ts/global/redirect";
 
-window.addEventListener('keydown', (e: KeyboardEvent) => {
-    switch (e.key) {
-        case 'ArrowUp':
-        case 'ArrowDown':
-        case 'ArrowLeft':
-        case 'ArrowRight':
-            e.preventDefault();//防止触发滚动等
-            fireMP(e.key);
-            break;
+window.onload=function(){
+    const gl=document.getElementById('gameLoading')!;
+    gl.style.display="none";
+    isLoaded=true;
+}
+let isLoaded = false;
+let gameStarted = false;
+export function startGame_click(){
+    if (!gameStarted && isLoaded) {
+        gameStarted = true;
+        document.getElementById('gameStart')!.style.display = 'none';
+        window.addEventListener('keydown', (e: KeyboardEvent) => {
+            switch (e.key) {
+                case 'ArrowUp':
+                case 'ArrowDown':
+                case 'ArrowLeft':
+                case 'ArrowRight':
+                    e.preventDefault();//防止触发滚动等
+                    fireMP(e.key);
+                    break;
+            }
+        });
+        fireCharge_Spawn();
     }
-});
+}
+
+
 //判断是否碰撞（精度范围：矩形）
 function isCollide(a:DOMRect, b:DOMRect) {
     return !(
@@ -38,34 +55,101 @@ function doCollideCheck(dataLoc:number[]): boolean {
     }
 }
 const mpSpeed=2;
-const player=document.getElementById("player");
+const player:HTMLElement|null=document.getElementById("player");
+const player_red:HTMLImageElement|null=document.querySelector("#player .red") as HTMLImageElement;
+let lastKey:string='null';
 function fireMP(key:string){
     if (player) {
-        switch (key) {
-            case 'ArrowUp':
-                mjyyProjectile_Move(player.offsetTop-player.offsetHeight+24,
-                    player.offsetLeft-player.offsetWidth+24,
-                    -mpSpeed,0,
-                    ++cumulativeSequence[0][0],[0,0]);
-                break;
-            case 'ArrowDown':
-                mjyyProjectile_Move(player.offsetTop+24,
-                    player.offsetLeft+24,
-                    mpSpeed,0,
-                    ++cumulativeSequence[0][1],[0,1]);
-                break;
-            case 'ArrowLeft':
-                mjyyProjectile_Move(player.offsetTop+24,
-                    player.offsetLeft-player.offsetWidth+24,
-                    0,-mpSpeed,
-                    ++cumulativeSequence[0][2],[0,2]);
-                break;
-            case 'ArrowRight':
-                mjyyProjectile_Move(player.offsetTop-player.offsetHeight+24,
-                    player.offsetLeft+24,
-                    0,mpSpeed,
-                    ++cumulativeSequence[0][3],[0,3]);
-                break;
+        if (canShootFire) {
+            switch (key) {
+                case 'ArrowUp': {
+                    async function doIt(wait: number = 0) {
+                        if (wait > 0) await sleep(wait);
+                        if (player) {
+                            await mjyyProjectile_Move(player.offsetTop - player.offsetHeight + 24 + 12,
+                                player.offsetLeft - player.offsetWidth + 24,
+                                -mpSpeed, 0,
+                                ++cumulativeSequence[0][0], [0, 0]);
+                        }
+                    }
+
+                    if (lastKey == 'ArrowUp') {
+                        player_doAnim("player-top-shoot .25s forwards");
+                        doIt();
+                    } else {
+                        player_doAnim("player-top .5s forwards");
+                        lastKey = 'ArrowUp';
+                        doIt(250);
+                    }
+                }
+                    break;
+                case 'ArrowDown': {
+                    async function doIt(wait: number = 0) {
+                        if (wait > 0) await sleep(wait);
+                        if (player) {
+                            mjyyProjectile_Move(player.offsetTop + 24 - 12,
+                                player.offsetLeft + 24 - 12,
+                                mpSpeed, 0,
+                                ++cumulativeSequence[0][1], [0, 1]);
+                        }
+                    }
+
+                    if (lastKey == 'ArrowDown') {
+                        player_doAnim("player-bottom-shoot .25s forwards");
+                        doIt();
+                    } else {
+                        player_doAnim("player-bottom .5s forwards");
+                        lastKey = 'ArrowDown';
+                        doIt(250);
+                    }
+                }
+                    break;
+                case 'ArrowLeft': {
+                    async function doIt(wait: number = 0) {
+                        if (wait > 0) await sleep(wait);
+                        if (player) {
+                            mjyyProjectile_Move(player.offsetTop + 24 - 12,
+                                player.offsetLeft - player.offsetWidth + 24,
+                                0, -mpSpeed,
+                                ++cumulativeSequence[0][2], [0, 2]);
+                        }
+                    }
+
+                    if (lastKey == 'ArrowLeft') {
+                        player_doAnim("player-left-shoot .25s forwards");
+                        doIt();
+                    } else {
+                        player_doAnim("player-left .5s forwards");
+                        lastKey = 'ArrowLeft';
+                        doIt(250);
+                    }
+                }
+                    break;
+                case 'ArrowRight': {
+                    async function doIt(wait: number = 0) {
+                        if (wait > 0) await sleep(wait);
+                        if (player) {
+                            mjyyProjectile_Move(player.offsetTop - player.offsetHeight + 24,
+                                player.offsetLeft + 24 - 12,
+                                0, mpSpeed,
+                                ++cumulativeSequence[0][3], [0, 3]);
+                        }
+                    }
+
+                    if (lastKey == 'ArrowRight') {
+                        player_doAnim("player-right-shoot .25s forwards");
+                        doIt();
+                    } else {
+                        player_doAnim("player-right .5s forwards");
+                        lastKey = 'ArrowRight';
+                        doIt(250);
+                    }
+                }
+                    break;
+                default:
+                    return;
+            }
+            shootHot();
         }
     }
 }
@@ -97,7 +181,7 @@ async function objMove(
         if (sequence==nowSequence[dataLoc[0]][dataLoc[1]]){
             if (!doCollideCheck(dataLoc))
                 break;
-            console.log(nowSequence);
+            //console.log(nowSequence);
             allLoc[dataLoc[0]][dataLoc[1]]=obj.getBoundingClientRect();
             if (dataLoc[0]==1 && player){
                 if (isCollide(obj.getBoundingClientRect(),player.getBoundingClientRect())){
@@ -134,7 +218,6 @@ async function fireCharge_Spawn(){
     while (true) {
         switch (Math.floor(Math.random() * 4)) {
             case 0:
-
                 fireCharge_Move(-24,
                     player.offsetLeft - player.offsetWidth + 24,
                     fcSpeed, 0,
@@ -143,13 +226,13 @@ async function fireCharge_Spawn(){
                 break;
             case 1:
                 fireCharge_Move(window.innerHeight,
-                    player.offsetLeft+24,
+                    player.offsetLeft+24-12,
                     -fcSpeed, 0,
                     2,0,
                     ++cumulativeSequence[1][1],[1,1]);
                 break;
             case 2:
-                fireCharge_Move(player.offsetTop + 24,
+                fireCharge_Move(player.offsetTop + 24-12,
                     -24,
                     0, fcSpeed,
                     0,1,
@@ -183,5 +266,118 @@ async function fireCharge_Move(
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    fireCharge_Spawn();
 });
+let isAnimating:boolean=false;
+let lastAnim:string = 'null';
+function player_doAnim(anim:string){
+    function freeze(){
+        if (player) {
+            const tra = getComputedStyle(player).transform;
+            player.style.transform = tra === 'none' ? 'unset' : tra;
+        }
+    }
+    if (player) {
+        if (isAnimating) {
+            player.style.animationPlayState = 'paused';
+            freeze();
+        } else {
+            player.addEventListener('animationend', function () {
+                //监听动画结束，动画结束后将当前状态固化
+                freeze();
+                isAnimating = false;
+            }, {once: true/*只执行一次监听*/});
+        }
+        if (lastAnim==anim) {
+            player.style.animation = 'unset';
+            void player.offsetWidth;//强制回流，强制让浏览器将上行更改应用
+        }
+        isAnimating = true;
+        player.style.animation = anim;
+        lastAnim=anim;
+    }
+}
+
+let canShootFire:boolean=true;
+let shootHotValue:number=0;//枪管热度值
+function shootHotValue_change(v:number,changeShow:boolean=true){
+    shootHotValue+=v;
+    if (shootHotValue<0)
+        shootHotValue=0;
+    if (changeShow) shootHotValue_changeShow();
+}
+//将枪管热度表现出来
+function shootHotValue_changeShow(){
+    if (player_red){
+        player_red.style.opacity=(shootHotValue/shootHotMax).toString();
+    }
+}
+const shootHotMax:number = 80;//枪管热度极限值
+//枪管过热计算
+function shootHot(){
+    if (shootHotValue++<=0){
+        console.log('shootCooling Run');
+        shootCooling();
+    }
+    shootHotValue_changeShow();
+}
+const coolingSpeed:number = 1;//冷却速度
+const coolingInterval:number=10;//冷却速度间隙
+const coolingByStopshoot:number=1000;//停火多久后开始冷却
+const coolingFlushTime:number = 10;//冷却计算刷新时间
+//枪管冷却计算
+async function shootCooling(){
+    let lastshootHotV:number=shootHotValue;//记忆上次的枪管热度，用于判断当前是否继续在开枪
+    let stopshootV:number=0;//停火时间值
+    while (shootHotValue>0){
+        if (shootHotValue>=shootHotMax && canShootFire){
+            canShootFire=false;
+        }
+        if (stopshootV>=coolingByStopshoot || !canShootFire){
+            console.log('cooling now');
+            const flashMax:number=10;//闪烁等待步进
+            let flashNow:number=0;
+            let cantShootWait_nowV:number = 0;//如果是无法射击，则等待停火一段时间后再进行恢复
+            do {
+                if (canShootFire || cantShootWait_nowV>=coolingByStopshoot) {
+                    if (canShootFire) {
+                        shootHotValue_change(-coolingSpeed);
+                        void player_red!.offsetWidth;
+                    }else{
+                        shootHotValue_change(-coolingSpeed,false);
+                        if (flashNow==Math.round(flashMax/3)){
+                            shootHotValue_changeShow();
+                            void player_red!.offsetWidth;
+                        }
+                    }
+                }else{
+                    cantShootWait_nowV+=coolingInterval;
+                    if (flashNow==Math.round(flashMax/3) && player_red) {
+                        player_red.style.opacity='1';
+                        void player_red.offsetWidth;
+                    }
+                }
+                lastshootHotV=shootHotValue;
+                if (!canShootFire){
+                    if(flashNow++>=flashMax){
+                        if (player_red){
+                            player_red.style.opacity=(Number(player_red.style.opacity)/2).toString();
+                            void player_red.offsetWidth;
+                        }
+                        flashNow=0;
+                    }
+                }
+                await sleep(coolingInterval);
+            }while (lastshootHotV==shootHotValue && !(shootHotValue<=0))
+            stopshootV=0;
+        }
+        if (lastshootHotV==shootHotValue){
+            stopshootV+=coolingFlushTime;
+            //console.log(stopshootV);
+        }else{
+            stopshootV=0;
+            lastshootHotV=shootHotValue;
+        }
+        await sleep(coolingFlushTime);
+    }
+    if (!canShootFire) canShootFire=true;//超出最大值后需等待完全冷却后才可继续设计
+}
